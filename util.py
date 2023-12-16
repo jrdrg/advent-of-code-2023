b0b3c2d90a710bb0e4a6ccf0d293b06f33e1991f
jrdrg <jrodriguez.0987+github@gmail.com>
2023-12-16 10:57:10 -0500

    day 16

--- util.py
+++ util.py
@@ -1,7 +1,18 @@
-def get_lines(inp: str) -> list[str]:
+def get_lines(inp: str, remove_blanks: bool = True) -> list[str]:
     lines = inp.split("\n")
-    lines = filter(lambda l: len(l) > 0, lines)
+    if remove_blanks:
+        lines = filter(lambda l: len(l) > 0, lines)
     return list(lines)
 
 def add(a: int, b: int):
     return a + b
+
+
+Grid = dict[tuple[int, int], str]
+
+def lines_to_grid(lines: list[str]) -> Grid:
+    grid = {}
+    for y, line in enumerate(lines):
+        for x, cell in enumerate(line):
+            grid[(y, x)] = cell
+    return grid
